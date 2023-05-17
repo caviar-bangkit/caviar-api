208b7026f16b0c3148b5daba711ea2b732dd50df
AhmadHasbyBik <abik262002@gmail.com>
2023-05-17 14:57:45 +0700

    routes

--- routes/crossing_route.js
+++ routes/crossing_route.js
@@ -2,9 +2,18 @@ const router = require("express").Router();
 const {
   getAllsCrossing,
   getCrossing,
+  getNearestCrossings,
+  createCrossing,
+  updateCrossing,
+  deleteCrossing,
 } = require("../controllers/crossing_controller");
 
 router.get("/crossings", getAllsCrossing);
 router.get("/crossings/:id", getCrossing);
+router.post("/getNearestCrossings", getNearestCrossings);
+router.post("/createCrossing/:id", createCrossing);
+router.post("/updateCrossing/:id", updateCrossing);
+router.delete("/deleteCrossing/:id", deleteCrossing);
+
 
 module.exports = router;
