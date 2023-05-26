ddda3e40be649b25a7a8732e476357e1bfa747df
Hanif Al Fathoni <alfatoni922@gmail.com>
2023-05-26 18:51:46 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const CrossingRoute = require("./routes/crossing_route");
+const middleware = require("./middleware");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(middleware.decodeToken);
 
 app.use("/api", CrossingRoute);
 
