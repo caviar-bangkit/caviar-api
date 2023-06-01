32383ff56b5a1baeb95a3487071f66debbf76459
Hanif Al Fathoni <alfatoni922@gmail.com>
2023-06-01 12:10:30 +0700

    Fixing middleware

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const connection = async (config) => {
   return mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.NAME,
+    database: process.env.DB_NAME,
     socketPath: process.env.INSTANCE_UNIX_SOCKET,
     ...config,
   });
