ac210958cf8731f9b670c60615d2aed2e5d38777
AhmadHasbyBik <abik262002@gmail.com>
2023-06-13 22:37:53 +0700

    edit, still not work

--- front-end/src/pages/editCrossings.js
+++ front-end/src/pages/editCrossings.js
@@ -1,43 +1,64 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import firebase from 'firebase/compat/app';
 
 export default function EditCrossings({ id, onSubmit }) {
   const [crossings, setCrossings] = useState({});
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    if (token) {
+      fetchData(token);
+    }
+  }, [token]);
 
-  const fetchData = async () => {
+  const fetchData = async (token) => {
     try {
-      const res = await axios.get(`https://caviar-api-qyyuck654a-et.a.run.app/api/crossing/${id}`);
+      const res = await axios.get(`https://caviar-api-qyyuck654a-et.a.run.app/api/crossings/${id}`, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
       setCrossings(res.data.data);
     } catch (error) {
       console.log(error);
-    }
+    } 
   };
 
   const handleUpdate = () => {
-    const data = {
-      id: crossings.id,
-      name: crossings.name,
-      latitude: crossings.latitude,
-      longitude: crossings.longitude,
-      heading: crossings.heading,
-    };
-
-    axios
-      .put(`https://caviar-api-qyyuck654a-et.a.run.app/api/crossing/${id}`, data)
-      .then((res) => {
-        console.log(res.data.data);
-        setCrossings(res.data.data);
-        onSubmit();
+    // Retrieve the access token
+    firebase.auth().currentUser.getIdToken(/* forceRefresh */ true)
+      .then(function(token) {
+        const data = {
+          id: crossings.id,
+          name: crossings.name,
+          latitude: crossings.latitude,
+          longitude: crossings.longitude,
+          heading: crossings.heading,
+        };
+  
+        // Make the PUT request with the access token
+        axios.put(`https://caviar-api-qyyuck654a-et.a.run.app/api/crossing/${id}`, data, {
+          headers: {
+          Authorization: "Bearer " + token,
+          },
+        })
+          .then((res) => {
+            console.log(res.data.data);
+            setCrossings(res.data.data);
+            onSubmit();
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       })
-      .catch((err) => {
-        console.log(err);
+      .catch(function(error) {
+        console.log(error);
+        // Handle error
       });
   };
+  
 
   const handleChange = (e) => {
     setCrossings({ ...crossings, [e.target.name]: e.target.value });
